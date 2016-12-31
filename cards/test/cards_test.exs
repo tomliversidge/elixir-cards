84e99e08f06b9daa65a55133483bc88da7c2715d
Tom Liversidge <tom@liversidge.me>
2016-12-31 10:38:18 +0200

    add basic functionality to Cards module

---
+++ cards/test/cards_test.exs
@@ -0,0 +1,25 @@
+defmodule CardsTest do
+  use ExUnit.Case
+  doctest Cards
+
+  test "deal returns correct number of cards" do
+    deck = ["1", "2", "3"]
+    {hand, remaining} = Cards.deal(deck, 1)
+    assert Enum.count(hand) == 1
+    assert Enum.count(remaining) == 2
+  end
+
+  describe "Cards.contains?" do
+
+    test "returns true when card in deck" do
+        deck = ["1", "2", "3"]
+        assert Cards.contains?(deck, "1")
+      end
+
+      test "returns false when card not in deck" do
+          deck = ["1", "2", "3"]
+          refute Cards.contains?(deck, "123123")
+      end
+  end
+
+end
